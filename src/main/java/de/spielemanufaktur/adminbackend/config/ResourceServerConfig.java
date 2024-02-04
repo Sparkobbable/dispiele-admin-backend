8a97723f81c597618ea1f6866437bcde986d7844
Jakob <j.wilmesmeier@gmail.com>
2024-02-04 19:39:08 +0100

    change resource config

--- src/main/java/de/spielemanufaktur/adminbackend/config/ResourceServerConfig.java
+++ src/main/java/de/spielemanufaktur/adminbackend/config/ResourceServerConfig.java
@@ -11,7 +11,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class ResourceServerConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.securityMatcher("/admin/**")
+        http.securityMatcher("/**")
                 .authorizeHttpRequests()
                 .requestMatchers("/oauthproxy/**").permitAll()
                 .requestMatchers("/admin/**").hasAuthority("ADMIN")
